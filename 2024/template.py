ca1ec7788333578335813f0b294911b3029a043b
Raymond <raymondliu4414@gmail.com>
2024-11-30 18:53:43 -0800

    2024 setup

---
+++ 2024/template.py
@@ -0,0 +1,29 @@
+import sys
+import pyperclip
+sys.path.append('../')
+import AoC_helpers
+
+def run(filename: str, part1: bool):
+    
+    return -1
+
+
+if __name__ == "__main__" :
+    if len(sys.argv) < 3:
+        print("Error, requires two command lines arguments: s/i 1/2")
+        exit()
+    if (sys.argv[1] != 's' and sys.argv[1] != 'i') or (sys.argv[2] != '1' and sys.argv[2] != '2'):
+        print("Error invalid command line args:", sys.argv[1], sys.argv[2])
+        exit()
+
+    filename = ""
+    if sys.argv[1] == 's':
+        filename = "sample.txt"
+    elif sys.argv[1] == 'i':
+        filename = "input.txt"
+
+    part1 = (sys.argv[2] == '1')
+    result = run(filename, part1)
+    print(result)
+    pyperclip.copy(str(result))
+    
