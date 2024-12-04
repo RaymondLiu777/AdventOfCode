25d34d0f2c4feb78f0cb627a8e8731e2a5b5352a
Raymond <raymondliu4414@gmail.com>
2024-12-03 22:03:01 -0800

    Updated Helper Functions for Grids and Directions

--- 2024/AoC_helpers.py
+++ 2024/AoC_helpers.py
@@ -1,3 +1,49 @@
+class Directions:
+
+    U = (0, 1)
+    D = (0, -1)
+    L = (-1, 0)
+    R = (1, 0)
+    UR = (1, 1)
+    UL = (-1, 1)
+    DR = (1, -1)
+    DL = (-1, -1) 
+
+    # Useful constants
+    FOUR_DIR = {
+        "U": U, "D": D, "L": L, "R": R
+    }
+
+    DIAGONAL_DIR = { 
+        "UR": UR, "UL": UL, "DR": DR, "DL": DL 
+    }
+
+    DIR = { **FOUR_DIR, **DIAGONAL_DIR }
+
+
+class Grid:
+    def __init__(self, grid):
+        self.grid = grid
+        self.rows = len(grid)
+        self.cols = len(grid[0])
+    
+    def Get(self, location, *argv):
+        newlocation = TupleOps.Add(location, *argv)
+        return self.grid[newlocation[0]][newlocation[1]]
+
+    def InGrid(self, location):
+        return location[0] >= 0 and location[1] >= 0 and location[0] < len(self.grid) and location[1] < len(self.grid[location[0]])
+
+class TupleOps:
+    def Add(tup1, *argv):
+        value = tup1
+        for arg in argv:
+            value = tuple(map(lambda i, j: i + j, value, arg))
+        return value
+
+    def Multiply(tup1, multiple):
+        return tuple(map(lambda i: i * multiple, tup1))
+
 def parseLine(string: str, *arg):
     string = string.strip()
     result = []
@@ -11,11 +57,5 @@ def parseLine(string: str, *arg):
         result.append(string[start:])
     return tuple(result)
 
-def Get2DLocation(grid, location):
-    return grid[location[0]][location[1]]
 
-def InGrid(grid_rows, grid_cols, location):
-    return location[0] >= 0 and location[1] >= 0 and location[0] < grid_rows and location[1] < grid_cols
 
-def NextLocation(location, offset):
-    return tuple(map(lambda i, j: i + j, location, offset))
