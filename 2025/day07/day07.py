3133860f49dd61f3beaf7c9b26ef4974ef1d2a0b
Raymond <raymondliu4414@gmail.com>
2025-12-06 21:16:54 -0800

    Day 7

---
+++ 2025/day07/day07.py
@@ -0,0 +1,57 @@
+import sys
+import pyperclip
+# import z3
+# import networkx as nx
+sys.path.append('../AoC_Helpers')
+from InputParser import InputParser
+from Grid import Directions, Grid
+# from TupleOps import TupleOps
+# from Graph import Graph
+# from functools import cache
+from collections import defaultdict
+
+
+def run(filename: str, part1: bool):
+    data = InputParser(open(filename).read()).readGrid().getData()
+    grid = Grid(data)
+    start = (-1, -1)
+    for location in grid:
+        if grid.Get(location) == "S":
+            start = location
+    cols = defaultdict(int)
+    cols[start[1]] = 1
+    splits = 0
+    for row in range(start[0] + 1, grid.rows):
+        next_cols = defaultdict(int)
+        for col, amount in cols.items():
+            if(grid.Get((row, col)) == "^"):
+                next_cols[col-1] += amount
+                next_cols[col+1] += amount
+                splits += 1
+            else:
+                next_cols[col] += amount
+        cols = next_cols
+    return splits if part1 else sum(cols.values())
+
+
+if __name__ == "__main__" :
+    if len(sys.argv) < 3:
+        print("Error, requires two command lines arguments: s/i 1/2")
+        exit()
+    if sys.argv[2] != '1' and sys.argv[2] != '2':
+        print("Error invalid command line args:", sys.argv[1], sys.argv[2])
+        exit()
+
+    filename = ""
+    if sys.argv[1] == 's':
+        filename = "sample.txt"
+    elif sys.argv[1] == 'i':
+        filename = "input.txt"
+    else:
+        filename = sys.argv[1]
+
+    part1 = (sys.argv[2] == '1')
+    result = run(filename, part1)
+    print(result)
+    pyperclip.copy(str(result))
+    
